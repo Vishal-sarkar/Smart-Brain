054b5a47725e78950ccf132a2240594991f71495
Vishal-sarkar <74032678+Vishal-sarkar@users.noreply.github.com>
2021-10-15 11:08:54 +0530

    deploy my app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const app = new Clarifai.App({
+ apiKey: '745b1518b208484aa403c058a6b294aa'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
 }
 
+ 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0]
+      .region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value});
+}
+
+onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(error => console.log(error));
+}
+
+  render() {
+    return (
+      <div className="App">
+           <Particles className = 'particles'
+              params={particlesOptions}
+            />
+          <Navigation/>
+          <Logo/>
+          <Rank/>
+          <ImageLinkForm 
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}
+          />
+          <FaceRecognition box={this.state.box} imageUrl={this.state.input}/>
+      </div>
+    );
+  }
+}
+ 
 export default App;
+
+
