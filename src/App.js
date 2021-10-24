e2a49e999b0f91a78d09567ac70444eabe283601
Vishal-sarkar <74032678+Vishal-sarkar@users.noreply.github.com>
2021-10-24 20:46:42 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ onInputChange = (event) => {
 
 onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://aqueous-oasis-03541.herokuapp.com/imageurl', {
            method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -90,7 +90,7 @@ onButtonSubmit = () => {
     .then(response => response.json())
     .then(response =>{
       if(response){
-        fetch('http://localhost:3000/image', {
+        fetch('https://aqueous-oasis-03541.herokuapp.com/image', {
            method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
